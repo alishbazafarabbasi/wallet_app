320526df38c61e427fc0f04983a4277f5ad02a73
Alishba Zafar Abbasi <alishbazafar.aur@gmail.com>
2024-08-01 23:01:14 +0500

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from config import Config
 from routes.wallet_routes import wallet_bp
 from routes.transaction_routes import transaction_bp
@@ -6,6 +6,20 @@ from routes.transaction_routes import transaction_bp
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(wallet_bp)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    try:
+        data = request.json
+        if not data:
+            raise ValueError("No JSON data received")
+
+        app.logger.info(f"Received data: {data}")
+        return jsonify({'message': 'Webhook received!'}), 200
+    except Exception as e:
+        app.logger.error(f"Error processing request: {e}")
+        return jsonify({'message': 'Error processing request'}), 400
+
 
 # Register Blueprints
 # app.register_blueprint(wallet_bp)
